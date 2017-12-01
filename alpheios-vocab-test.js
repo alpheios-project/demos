a9b5b0f19cbe4b793a4d7d374ee5cec78dec0537
Bridget Almas <balmsa@gmail.com>
2017-12-01 15:41:21 -0500

    ported from alpheios.net server

---
+++ alpheios-vocab-test.js
@@ -0,0 +1,80 @@
+var reposUrl = "http://repos1.alpheios.net/exist3/rest/db/xq";
+
+$(function() {
+                $("#showkeylink").click(showkey);
+                $("#vocabfmtlink").click(showvocabfmt);
+                $("#textfmtlink").click(showtextfmt);
+                $("form").submit(
+                    function()
+                    {
+                        $("input[name=excludepofs]:checked").each(
+                            function() { $(this).val("1") });
+                        $("input[name=missed]:checked").each(
+                            function() { $(this).val("1") });
+                        if ($("input.xmlFormat:checked",this).length >0) 
+                        {
+                            $("input[name=format]",this).val("xml");
+                        } 
+                        else
+                        {
+                            $("input[name=format]",this).val("html");
+                        }
+                        if ($("input[name=doc]",this).length > 0)
+                        {
+                            $("input[name=docUrn]",this).remove();
+                        }
+                        this.submit();
+                        return false;
+                    }
+                );
+                
+});                            
+
+function showkey()
+{
+    $(".dropkey").toggleClass("show");        
+    return false;
+}
+function showvocabfmt()
+{
+    $(".vocabfmt").toggleClass("show");        
+    return false;
+}
+function showtextfmt()
+{
+    $(".textfmt").toggleClass("show");        
+    return false;
+}
+
+function getInventory()
+{
+    $.ajax({
+                    type: "GET",
+                    async: true,
+                    dataType: "xml",
+                    cache: false,
+                    url: reposUrl + "/alpheios-get-toc.xq?q=getCapabilities",
+                    success:  getTexts,
+                     error:
+                        function(a_req,a_text,a_error){alert("Error: " + a_error + ":" + a_text)}
+                })                
+}
+
+function getTexts(a_data,a_status)
+{
+    alert(a_data);
+    $("work",a_data).each(
+        function()
+        {
+            alert($("title",this));
+        }
+    )
+}
+function populateSelects()
+{
+                $("select.inventory").each(
+                    function()
+                    {
+                    }
+                );               
+}
